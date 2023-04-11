dd5d1ef65195f67c66263b36bcad48121ec05dc1
Abel Abraham Estrella Ojeda <aaeojeda@gmail.com>
2023-04-11 00:55:01 -0600

    Primera Sincronización

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CursoController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', HomeController::class);
+
+Route::get('/cursos', [CursoController::class, 'index']);
+
+Route::get('/cursos/create',[CursoController::class, 'create']);
+
+Route::get('/cursos/{curso}', [CursoController::class, 'show']);
+
+/*
+Route::get('/cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
+    if ($categoria){
+        return "Bienvenido al curso ".$curso.", de la categoría ".$categoria;
+    }    
+    else {
+        return "Bienvenido al curso: ".$curso;
+    }
+});
+*/
